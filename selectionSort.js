ec9df14fd2ea14e6a70747a24843457c49e7841e
Josh Powell <josh.powell@softwire.com>
2018-03-19 13:17:28 +0000

    merge sort

--- selectionSort.js
+++ selectionSort.js
@@ -41,5 +41,40 @@ function insertionSort(unsortedList) {
     }
     return sortedList;
 }
+function mergeSort(unsortedList) {
+    var listOfLists = makeListOfLists(unsortedList);
+    while (listOfLists.length > 1) {
+        var newList = [];
+        while (listOfLists.length >= 1) {
+            newList.push(mergeLists(listOfLists.shift(), listOfLists.shift()));
+        }
+        listOfLists = newList;
+    }
+    return listOfLists[0];
+}
+function mergeLists(listOne, listTwo) {
+    var combinedList = [];
+    if (listTwo === undefined) {
+        return listOne;
+    }
+    while (listOne.length > 0 || listTwo.length > 0) {
+        if (listOne[0] <= listTwo[0] || listTwo.length === 0) {
+            combinedList.push(listOne.shift());
+        }
+        else {
+            combinedList.push(listTwo.shift());
+        }
+    }
+    return combinedList;
+}
+function makeListOfLists(list) {
+    var listOfLists = [];
+    for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+        var number = list_1[_i];
+        listOfLists.push([number]);
+    }
+    return listOfLists;
+}
 console.log(selectionSort([5, 6, 7, 1, 4, 3, 7, 1]));
-console.log(insertionSort([11, 6, 7, 1, 4, 3, 7, 1, 9, 102, 2, 40, 1, 5, 4, 5, 4]));
+console.log(insertionSort([11, 6, 7, 1, 4, 3, 7, 1, 9, 102, 2, 40, 1, 5, 4, 5, 4, 0]));
+console.log(mergeSort([11, 6, 7, 1, 4, 3, 7, 1, 9, 102, 2, 40, 1, 5, 4, 5, 4, 0]));
--- selectionSort.ts
+++ selectionSort.ts
@@ -31,23 +31,62 @@ function insertionSort(unsortedList: Array<number>) {
         };
         let placed = false;
 
-        for (let j = 0; j <= sortedList.length; j++){
+        for (let j = 0; j <= sortedList.length; j++) {
 
-            if (sortedList[j] > currentNumber.value || sortedList.length === 0){
+            if (sortedList[j] > currentNumber.value || sortedList.length === 0) {
                 sortedList.splice(j, 0, currentNumber.value);
                 placed = true;
                 break;
             }
 
         }
-        if (!placed){
+        if (!placed) {
             sortedList.push(currentNumber.value);
         }
     }
     return sortedList;
 }
 
+function mergeSort(unsortedList: Array<number>) {
+    let listOfLists = makeListOfLists(unsortedList);
+
+    while (listOfLists.length > 1) {
+        let newList = [];
+        while (listOfLists.length >= 1) {
+            newList.push(mergeLists(listOfLists.shift(), listOfLists.shift()));
+        }
+        listOfLists = newList
+    }
+
+    return listOfLists[0];
+}
+
+function mergeLists(listOne: Array<number>, listTwo?: Array<number>) {
+    let combinedList = [];
+    if (listTwo === undefined) {
+        return listOne;
+    }
+    while (listOne.length > 0 || listTwo.length > 0) {
+
+        if (listOne[0] <= listTwo[0] || listTwo.length === 0) {
+            combinedList.push(listOne.shift());
+        } else {
+            combinedList.push(listTwo.shift());
+        }
+    }
+    return combinedList;
+}
+
+function makeListOfLists(list: Array<number>) {
+    let listOfLists = [];
+
+    for (let number of list) {
+        listOfLists.push([number]);
+    }
+    return listOfLists;
+}
 
 console.log(selectionSort([5, 6, 7, 1, 4, 3, 7, 1]));
-console.log(insertionSort([11, 6, 7, 1, 4, 3, 7, 1, 9, 102, 2, 40, 1, 5, 4, 5, 4]));
+console.log(insertionSort([11, 6, 7, 1, 4, 3, 7, 1, 9, 102, 2, 40, 1, 5, 4, 5, 4, 0]));
+console.log(mergeSort([11, 6, 7, 1, 4, 3, 7, 1, 9, 102, 2, 40, 1, 5, 4, 5, 4, 0]));
 
